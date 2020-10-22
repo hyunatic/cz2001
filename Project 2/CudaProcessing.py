1e43af6b9daa18c3fe3c754f566855a23f25c764
hyunatic <habbo_armageddon@hotmail.com>
2020-10-22 17:04:07 +0800

    Explained Codes

--- Project 2/CudaProcessing.py
+++ Project 2/CudaProcessing.py
@@ -7,19 +7,24 @@ from timeit import default_timer as timer
 #My is running on 
 
 def ReadFile():
+    #Read VIA CPU and store into RAM
     edge = np.loadtxt("roadNet-PA.txt", dtype="int32", skiprows=4)
     array = np.array(edge)
     return array
 
 def FullAdjMatrix(arr1):
+    #Device 1 is Asus Strix RTX 3090
+    #cp.cuda.Device(1).use()
     size = cp.unique(arr1).size
     cpSortedArr = arr1[arr1[:,0].argsort()]
 
+    #Device 0 is MSI Gaming X Trio RTX 3090
+    #cp.cuda.Device(0).use()
     arr = cp.zeros(size, dtype="int32")
     cur = 0
+    #Don't uncomment this line, I'm using 2 GPU(s) on SLI to process
+    #cp.cuda.Device({0,1}).use()
     with open("FullmatrixList.txt", 'w') as f:
-        #Don't uncomment this line, I'm using RTX 3090 on SLI to process
-        #cp.cuda.Device(0,1).use()
         for row,col in cpSortedArr:
             if(cur != row):
                 np.savetxt("FullmatrixList.txt", cp.asnumpy(arr),header="[", footer="]", newline=" ", fmt="%s")
@@ -30,14 +35,19 @@ def FullAdjMatrix(arr1):
             np.savetxt("FullmatrixList.txt", cp.asnumpy(arr), newline=" ", fmt="%s")
 
 def CompressedAdjMatrix(arr1):
+    #Device 1 is Asus Strix RTX 3090
+    #cp.cuda.Device(1).use()
     size = cp.unique(arr1).size
     cpSortedArr = arr1[arr1[:,0].argsort()]
 
+    #Device 0 is MSI Gaming X Trio RTX 3090
+    #cp.cuda.Device(0).use()
     arr = cp.zeros(size, dtype="int32")
     cur = 0
+
+    #Don't uncomment this line, I'm using 2 GPU(s) on SLI to process
+    #cp.cuda.Device({0,1}).use()
     with open("matrixList.txt", 'w') as f:
-        #Don't uncomment this line, I'm using RTX 3090 on SLI to process
-        #cp.cuda.Device(0,1).use()
         for row,col in cpSortedArr:
             if(cur != row):
                 f.write(cp.array_str(arr) + "\n")
@@ -49,13 +59,22 @@ def CompressedAdjMatrix(arr1):
             f.close() 
 
 def ConvertToAdjList(arr1):
+    #Device 1 is Asus Strix RTX 3090
+    #cp.cuda.Device(1).use()
     cpSortedArr = arr1[arr1[:,0].argsort()]
     start,end = cp.hsplit(cpSortedArr, 2)
     node = cp.unique(cpSortedArr).size
- 
+    
+    #Device 0 is MSI Gaming X Trio RTX 3090
     #cp.cuda.Device(0).use()
     adjList = [[] for k in range(node)]
+
+    #Line 52 ~ 58 are executed in parallel
+    #Line 62 synchronizes my 2x 3090 on before continue the next part of the program
+    #cp.cuda.Device({0,1}).synchronize()
     
+    #Using both GPU to process at the same time
+    #cp.cuda.Device({0,1}).use()
     for i in range(node):
         u = int(start[i].get())
         v = int(end[i].get())
@@ -64,8 +83,10 @@ def ConvertToAdjList(arr1):
     np.savetxt("adjList.txt", adjList, delimiter=" ", fmt="%s")
 
 if __name__=="__main__":
+    #Read file data into RAM
     nparray = ReadFile()
 
+    #Transfer data into the GPU
     cparray = cp.array(nparray)
 
     #WARNING! DO NOT RUN THIS LINE UNLESS YOU WANT FULL MATRIX (1 MILLION X 1 MILLION)
